67f5fb490c7bc3545857f211c61af1f4bbaa05ae
Abby Beizer <abby@regression.gg>
2023-10-10 17:58:06 -0400

    Changes made from the Regression Games Agent Builder

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 // Base Class Nodes
-import { NodeStatus } from './lib/BaseClasses/NodeStatus';
+import { NodeStatus } from "./lib/BaseClasses/NodeStatus";
 import Node from "./lib/BaseClasses/Node";
 import RootNode from './lib/BaseClasses/RootNode';
 import SequenceNode  from './lib/BaseClasses/SequenceNode';
@@ -11,235 +11,61 @@ import AlwaysSucceed from './lib/BaseClasses/Decorators/AlwaysSucceed';
 import AlwaysFail from './lib/BaseClasses/Decorators/AlwaysFail';
 import Invert from './lib/BaseClasses/Decorators/Invert';
 
-// Generated Nodesimport IsInGame from './lib/IsInGame';
-    import SetChartype from './lib/SetChartype';
-    import IsFloorSwitchInRange from './lib/IsFloorSwitchInRange';
-    import StandOnSwitch from './lib/StandOnSwitch';
-    import IsHumanNearby from './lib/IsHumanNearby';
-    import FollowHuman from './lib/FollowHuman';
-    import IsEnemyNearby from './lib/IsEnemyNearby';
-    import Attack from './lib/Attack';
-    // Reference to root node
+// Generated Nodes
+import IsInGame from './lib/IsInGame';
+import SetChartype from './lib/SetChartype';
+import IsFloorSwitchInRange from './lib/IsFloorSwitchInRange';
+import StandOnSwitch from './lib/StandOnSwitch';
+import IsHumanNearby from './lib/IsHumanNearby';
+import FollowHuman from './lib/FollowHuman';
+import IsEnemyNearby from './lib/IsEnemyNearby';
+import Attack from './lib/Attack';
 
-
-
-
-
-
-
-
-
-
-
-
+// Reference to root node
 let rootNode: RootNode;
-    
-
 
 export function configureBot(rg: any) {
 
-
-    
-        rg.isSpawnable=true
-        rg.lifecycle="MANAGED"
-        
-            rg.characterConfig={
+    rg.isSpawnable=true
+    rg.lifecycle="MANAGED"
+    rg.characterConfig={
   "characterType": "Archer"
 }
-        
-    
 
+    // construct tree
+    const floorSwitches = new SequenceNode("Floor switches");
+    floorSwitches.addChild(new IsFloorSwitchInRange());
+    floorSwitches.addChild(new StandOnSwitch());
 
-// construct tree
+    const supportHuman = new SequenceNode("Support human");
+    supportHuman.addChild(new IsHumanNearby());
+    supportHuman.addChild(new FollowHuman());
 
-    
+    const attackEnemy = new SequenceNode("Attack enemy");
+    attackEnemy.addChild(new IsEnemyNearby());
+    attackEnemy.addChild(new Attack());
 
-    
+    const chooseAnAction = new SelectorNode("Choose an action");
+    chooseAnAction.addChild(floorSwitches);
+    chooseAnAction.addChild(supportHuman);
+    chooseAnAction.addChild(attackEnemy);
 
-    
+    const topLevelSequenceNode = new SequenceNode("Top Level Sequence Node");
+    topLevelSequenceNode.addChild(new IsInGame());
+    topLevelSequenceNode.addChild(new SetChartype());
+    topLevelSequenceNode.addChild(chooseAnAction);
 
-    
-
-    
-        
-        
-            const floorSwitches = new SequenceNode("Floor switches");
-        
-        
-            
-                floorSwitches.addChild(new IsFloorSwitchInRange());
-            
-            
-        
-            
-                floorSwitches.addChild(new StandOnSwitch());
-            
-            
-        
-
-    
-
-    
-
-    
-
-    
-        
-        
-            const supportHuman = new SequenceNode("Support human");
-        
-        
-            
-                supportHuman.addChild(new IsHumanNearby());
-            
-            
-        
-            
-                supportHuman.addChild(new FollowHuman());
-            
-            
-        
-
-    
-
-    
-
-    
-
-    
-        
-        
-            const attackEnemy = new SequenceNode("Attack enemy");
-        
-        
-            
-                attackEnemy.addChild(new IsEnemyNearby());
-            
-            
-        
-            
-                attackEnemy.addChild(new Attack());
-            
-            
-        
-
-    
-
-    
-        
-        
-            const chooseAnAction = new SelectorNode("Choose an action");
-        
-        
-            
-            
-                
-                
-                    chooseAnAction.addChild(floorSwitches);
-                
-            
-        
-            
-            
-                
-                
-                    chooseAnAction.addChild(supportHuman);
-                
-            
-        
-            
-            
-                
-                
-                    chooseAnAction.addChild(attackEnemy);
-                
-            
-        
-
-    
-
-    
-        
-        
-            const topLevelSequenceNode = new SequenceNode("Top Level Sequence Node");
-        
-        
-            
-                topLevelSequenceNode.addChild(new IsInGame());
-            
-            
-        
-            
-                topLevelSequenceNode.addChild(new SetChartype());
-            
-            
-        
-            
-            
-                
-                
-                    topLevelSequenceNode.addChild(chooseAnAction);
-                
-            
-        
-
-    
-
-    
-        
-            rootNode = new RootNode("Root Node");
-        
-        
-        
-            
-            
-                
-                
-                    rootNode.addChild(topLevelSequenceNode);
-                
-            
-        
-
-    
+    rootNode = new RootNode("Root Node");
+    rootNode.addChild(topLevelSequenceNode);
 
 }
 
 export async function processTick(rg: any) {
-try
-{
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
+    try
+    {
         await rootNode.execute(rg);
-    
-
-}
-catch(err) {
-console.log("Error Executing Tree: ", err)
-}
+    }
+    catch(err) {
+        console.log("Error Executing Tree: ", err)
+    }
 }
