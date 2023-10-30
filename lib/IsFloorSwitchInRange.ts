98044dfcfafc700822b46a87cd42d47e4dec96b8
Abby Beizer <abby@regression.gg>
2023-10-30 08:53:19 -0400

    Changes made from the Regression Games Agent Builder

--- lib/IsFloorSwitchInRange.ts
+++ lib/IsFloorSwitchInRange.ts
@@ -14,7 +14,7 @@ export default class IsFloorSwitchInRange extends ConditionNode {
         */
 	public override async execute(rg): Promise<NodeStatus>{		
 		// if the bot is standing on a switch, then do nothing
-		const floorSwitch = await rg.findEntity("FloorSwitch");
+		const floorSwitch = await rg.findNearestEntity("FloorSwitch", null, (entity) => {return rg.MathFunctions.distanceSq(rg.getBot().position, entity.position < 30)});		
 		if(floorSwitch) {
 			this.setData("floorSwitch", floorSwitch);
 			return NodeStatus.SUCCESS;
