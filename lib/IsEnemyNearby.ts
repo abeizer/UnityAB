466210ecb5748b8750b7499b990673260aeea2ce
Abby Beizer <abby@regression.gg>
2023-10-30 08:13:03 -0400

    Changes made from the Regression Games Agent Builder

--- lib/IsEnemyNearby.ts
+++ lib/IsEnemyNearby.ts
@@ -13,7 +13,8 @@ export default class IsEnemyNearby extends ConditionNode {
         * 
         */
 	public override async execute(rg): Promise<NodeStatus>{		
-		const enemy = await rg.findNearestEntity("Imp", rg.getBot().position, (entity) => { return entity.team === 1 && !entity.broken});
+		// null entity because we want either Imp or Crystal, whichever is closer
+		const enemy = await rg.findNearestEntity(null, null, (entity) => { return entity.team == 1 && !entity.broken});
 		if(enemy) {
 			this.setData("enemy", enemy);
 			return NodeStatus.SUCCESS;
