10c1522cc28af86ccf8cf94961c1b7a51ececa46
Abby Beizer <abby@regression.gg>
2023-10-31 12:05:39 -0400

    Changes made from the Regression Games Agent Builder

--- Nodes/FollowHuman.cs
+++ Nodes/FollowHuman.cs
@@ -26,7 +26,15 @@ namespace BossroomAb
          */
         protected override NodeStatus Execute(RG rgObject)
         {
-            return NodeStatus.SUCCESS;
+        	var closestHuman = GetData<RGStateEntity>("closestHuman");
+        	var followAction = new RGActionRequest("FollowObject", new Dictionary<string, object>
+        	{
+        		{ "targetId", closestHuman.id },
+        		{ "range", 2f }
+        	});
+        
+        	rgObject.PerformAction(followAction);
+        	return NodeStatus.Success;
         }
     }
 }
