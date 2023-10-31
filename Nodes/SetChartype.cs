545d8406fa88111b6e9357dd52e1393bd6dab88d
Abby Beizer <abby@regression.gg>
2023-10-31 08:00:26 -0400

    Changes made from the Regression Games Agent Builder

--- Nodes/SetChartype.cs
+++ Nodes/SetChartype.cs
@@ -22,28 +22,19 @@ namespace BossroomAb
 
         /**
          * Generated from prompt:
-         * 
+         * Store my bot's characterType in the data map. If characterType is not defined then fail.
          */
-        class LeafNode extends TreeNode {
-        	
-        	charInfo = {
-        		type: ["Mage", "Rogue", "Tank", "Archer"],
-        		abilities: [[0,1], [0,1,2], [0,1], [0,1,2]],
-        		// teamId
-        		abilityTargets: [[1,0], [1,1,-1], [1,-1], [1,1,1]]
+        protected override NodeStatus Execute(RG rgObject)
+        {
+        	var myPlayer = rgObject.GetMyPlayer();
+        	if(myPlayer.ContainsKey("characterType"))
+        	{
+        		SetData("characterType", myPlayer["characterType"]);
+        		return NodeStatus.Success;
         	}
-        
-        	public override async execute(rg): Promise<NodeStatus>{		
-        		// The character type we request may not be the one we actually get
-        		const characterType = rg.characterConfig.characterType;
-        		if (characterType) {
-        			const newCharType = this.charInfo.type.indexOf(characterType);
-        			this.setData("charType", newCharType)
-        			return NodeStatus.SUCCESS;
-          	}
-        		
-        		console.error("NO CHARACTER TYPE");
-        		return NodeStatus.FAILURE;
+        	else
+        	{
+        		return NodeStatus.Failure;
         	}
         }
     }
